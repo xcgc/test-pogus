d8614be131a9e8122580c31f499081e224d908f9
ILoveCatto <89790616+ILoveCatto@users.noreply.github.com>
2021-10-23 06:17:31 +0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,7 +6,7 @@ sudo mdutil -i off -a
 #Create new account
 sudo dscl . -create /Users/vncuser
 sudo dscl . -create /Users/vncuser UserShell /bin/bash
-sudo dscl . -create /Users/vncuser RealName "VNC User"
+sudo dscl . -create /Users/vncuser RealName "Crescent"
 sudo dscl . -create /Users/vncuser UniqueID 1001
 sudo dscl . -create /Users/vncuser PrimaryGroupID 80
 sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
